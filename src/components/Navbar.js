c0e4098795374b2bb234b9839028fbbe169b3551
JC Grennan <jgrennan94@gmail.com>
2022-08-22 19:22:21 +0100

    jsx composed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,34 @@ import "./Navbar.css"
 import {Link} from "react-router-dom"
 
 export default function Navbar() {
+
     return (
+
         <div className="nav">
+
             <Link to="/">
                 <h1>Portfolio</h1>
             </Link>
+
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+
+                <li>
+                    <Link to="/project">Project</Link>
+                </li>
+
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+
         </div>
+        
     )
 }
