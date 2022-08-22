76456093b6658947aee31251a8f9864253ea339b
JC Grennan <jgrennan94@gmail.com>
2022-08-22 19:16:21 +0100

    imported Link from react router and linked to Home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,2 +1,12 @@
 import "./Navbar.css"
+import {Link} from "react-router-dom"
 
+export default function Navbar() {
+    return (
+        <div className="nav">
+            <Link to="/">
+                <h1>Portfolio</h1>
+            </Link>
+        </div>
+    )
+}
