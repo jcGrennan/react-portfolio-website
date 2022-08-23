b745c6ce68e6b0b3044e4710f7fb3b67d28462fa
JC Grennan <jgrennan94@gmail.com>
2022-08-23 14:49:17 +0100

    import and rendered Hero

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,11 @@
-import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar"
+import HeroImg from "../components/HeroImg"
 
 export default function Home() {
     return (
         <div>
             <Navbar />
+            <HeroImg />
         </div>
     )
 }
