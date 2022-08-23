ae50f33c7a140c9ea0f455e61265987845a34cc4
JC Grennan <jgrennan94@gmail.com>
2022-08-23 19:52:05 +0100

    updated projects path

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/project" element={<Project />}/>
+        <Route path="/projects" element={<Project />}/>
         <Route path="/about" element={<About />}/>
         <Route path="/contact" element={<Contact />}/>
       </Routes>
