df95c9db14127617795a1fa976e7e33dff9571de
JC Grennan <jgrennan94@gmail.com>
2022-08-23 20:02:31 +0100

    Hero compnent renamed

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,12 @@
 import Navbar from "../components/Navbar"
-import HeroImg from "../components/HeroImg"
+import Hero from "../components/HeroImg"
 import Footer from "../components/Footer"
 
 export default function Home() {
     return (
         <div>
             <Navbar />
-            <HeroImg />
+            <Hero />
             <Footer />
         </div>
     )
