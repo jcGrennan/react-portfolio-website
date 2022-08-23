d655d91701a7540f5a9d67e310f86b48ea64f5b2
JC Grennan <jgrennan94@gmail.com>
2022-08-23 20:28:21 +0100

    passing Hero props

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,13 @@
 import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
 import Footer from "../components/Footer"
+import HomeImg from "../assets/home-img.jpg"
 
 export default function Home() {
     return (
         <div>
             <Navbar />
-            <Hero />
+            <Hero home={true} img={HomeImg}/>
             <Footer />
         </div>
     )
