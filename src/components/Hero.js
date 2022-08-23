4800c3f0ba5e028e3c21a88649b11d5c91a5306f
JC Grennan <jgrennan94@gmail.com>
2022-08-23 20:27:58 +0100

    making hero reusable

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,27 +1,27 @@
 import "./Hero.css"
-import HomeImg from "../assets/home-img.jpg"
 import {Link} from "react-router-dom"
 
-export default function Hero() {
+export default function Hero(props) {
     return (
         <div className="hero">
 
             <div className="hero--mask">
-                <img className="intro-img" src={HomeImg} alt="low lit desk and computer"/>
+                <img className="intro-img" src={props.img} alt="low lit desk and computer"/>
             </div>
 
             <div className="content">
 
-                <p>WELCOME TO MY PORTFOLIO</p>
-                <h1>React & JS Developer.</h1>
-                <div>
+                {props.home ? <p>WELCOME TO MY PORTFOLIO</p> : <h1>{props.heading}</h1>}
+                {props.home ? <h1>React & JS Developer.</h1> : <p>{props.text}</p>}
+                
+                {props.home && <div>
                     <Link to="/projects" className="btn">
                         Projects
                     </Link>
                     <Link to="/contact" className=" btn btn-light">
                         Contact
                     </Link>
-                </div>
+                </div>}
 
             </div>
 
