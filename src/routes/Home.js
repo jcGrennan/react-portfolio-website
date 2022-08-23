14f8956fdddd82ceb3c89f66df26d5694afea4ff
JC Grennan <jgrennan94@gmail.com>
2022-08-23 16:04:07 +0100

    import and rendered Footer

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar"
 import HeroImg from "../components/HeroImg"
+import Footer from "../components/Footer"
 
 export default function Home() {
     return (
         <div>
             <Navbar />
             <HeroImg />
+            <Footer />
         </div>
     )
 }
