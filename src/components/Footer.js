98ae3a8c4cc1a64a118985d298e950ef11336b39
JC Grennan <jgrennan94@gmail.com>
2022-08-23 16:04:23 +0100

    composed Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,75 @@
+import "./Footer.css"
+import {
+    FaHome, 
+    FaPhone, 
+    FaMailBulk, 
+    FaLinkedin, 
+    FaGithubSquare
+} from "react-icons/fa"
+
+export default function Footer() {
+    return (
+        <div className="footer">
+
+            <div className="footer--container">
+
+                <div className= "footer--left">
+
+                    <div className="footer--location">
+                        <FaHome  
+                            size={20} 
+                            style={{color:"#fff", marginRight:"2rem"}}
+                        />
+                    </div>
+
+                    <div>
+                        <p>Cork City</p>
+                        <p>Ireland</p>
+                    </div>
+
+                    <div className="footer--phone">
+                        <h4>
+                            <FaPhone 
+                                size={20} 
+                                style={{color:"#fff",                  marginRight:"2rem"}} 
+                            />
+                            +353-87-711-4012
+                        </h4>
+                    </div>
+
+                    <div className="footer--email">
+                        <h4>
+                            <FaMailBulk 
+                                size={20} 
+                                style={{color:"#fff", marginRight:"2rem"}} 
+                            />
+                            jgrennan94@gmail.com
+                        </h4>
+                    </div>
+
+                </div>
+
+                <div className="footer--right">
+
+                    <h4>About</h4>
+                    <p>Front-end developer with a natural aptitude for coding. I am excited by the prospect of committing to a position where I can combine my new skills with my proven abilities in management, communication and problem-solving.</p>
+
+                    <div className="social">
+                        <FaLinkedin 
+                            size={30} 
+                            style={{color:"#fff", marginRight:"1rem"}}
+                        />
+                        <FaGithubSquare
+                            size={30}
+                            style={{color:"#fff", marginRight:"1rem"}}
+                        />
+                        
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
