b9922b7347aa334d2d2546719537012e75ab2381
JC Grennan <jgrennan94@gmail.com>
2022-08-25 15:30:09 +0100

    composed and exported data for project card

---
+++ src/projectData.js
@@ -0,0 +1,66 @@
+import project1 from "./assets/project1.png"
+import project2 from "./assets/project2.png"
+import project3 from "./assets/project3.png"
+import project4 from "./assets/project4.png"
+import project5 from "./assets/project5.png"
+import project6 from "./assets/project6.png"
+
+const data = [
+
+    {
+        id: 1,
+        img: project1,
+        title: "Questionable - the Quiz",
+        details: "A multiple-choice quiz with category and difficulty options showcasing the use of React Hooks such as State, Effect, and Callback, and the use of props and reusable components.",
+        viewUrl: "url.com",
+        sourceUrl: "url.com",
+    },
+
+    {
+        id: 2,
+        img: project2,
+        title: "Tenzies Game",
+        details: "A dice match game showing the use of State, props, reusable components, and javaScript algorithms.",
+        viewUrl: "url.com",
+        sourceUrl: "url.com",
+    },
+
+    {
+        id: 3,
+        img: project3,
+        title: "Meme-Generator",
+        details: "A meme-generator that fetchs popular meme images from an api and allows the use to create the text in roulette style fun.",
+        viewUrl: "url.com",
+        sourceUrl: "url.com",
+    },
+
+    {
+        id: 4,
+        img: project4,
+        title: "My Travel Journal",
+        details: "A small one page travel journal showcasing the use of reusable components with props, and my ability to follow design files while scaling between mobile and wide screen devices.",
+        viewUrl: "url.com",
+        sourceUrl: "url.com",
+    },
+
+    {
+        id: 5,
+        img: project5,
+        title: "Air-Bnb Clone",
+        details: "A small one page air-bnb experience app showcasing the use of reusable components with props, and my ability to follow design files while scaling between mobile and wide screen devices.",
+        viewUrl: "url.com",
+        sourceUrl: "url.com",
+    },
+
+    {
+        id: 6,
+        img: project6,
+        title: "Digital Business Card",
+        details: "A digital business card showcasing some basic jsx composition, and my ability to follow design files while scaling between mobile and wide screen devices.",
+        viewUrl: "url.com",
+        sourceUrl: "url.com",
+    },
+    
+]
+
+export default data
