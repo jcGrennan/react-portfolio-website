0bac46002792f220e822900c8650b5857d10cdde
JC Grennan <jgrennan94@gmail.com>
2022-08-25 18:35:54 +0100

    composed and styled form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,26 @@ import "./Form.css"
 
 export default function Form() {
     return (
-        <div></div>
+
+        <div className="form">
+
+            <form>
+
+                <label for="name">Your Name</label>
+                <input id="name" type="text"></input>
+
+                <label for="email">Email</label>
+                <input id="email" type="email"></input>
+
+                <label for="subject">Subject</label>
+                <input id="subject" type="text"></input>
+
+                <label for="message">Message</label>
+                <textarea id="message" rows="6" placeholder="Type your message here" />
+
+                <button className="btn">Submit</button>
+            </form>
+
+        </div>
     )
 }
