c5d097a88d1df59c4c363576f0ec28913d455d6d
JC Grennan <jgrennan94@gmail.com>
2022-08-25 15:30:56 +0100

    made ProjectCard component reusable

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,32 @@
+import "./ProjectCard.css"
+import {NavLink} from "react-router-dom"
+
+export default function ProjectCard(props) {
+    return (
+
+            <div className="project-container">
+                <div className="project-card">
+
+                    <img src={props.img} alt="project screenshot" />
+
+                    <h2 className="project-title">{props.title}</h2>
+
+                    <div className="project-details">
+
+                        <p>{props.details}</p>
+
+                        <div className="project-btns">
+
+                            <NavLink to={props.viewUrl} className ="btn">View</NavLink>
+                       
+                            <NavLink to={props.sourceUrl} className ="btn">Source</NavLink>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+    )
+}
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,12 +1,25 @@
 import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
+import ProjectCard from "../components/ProjectCard"
+import data from "../projectData.js"
 import Footer from "../components/Footer"
 
 export default function Projects() {
+
+    const projectElements = data.map(project => {
+        return (
+            <ProjectCard
+                key={project.id}
+                {...project}
+            />
+        )
+    })
+
     return (
         <div>
             <Navbar />
             <Hero heading="PROJECTS." text="Some of my latest work"/>
+            {projectElements}
             <Footer />
         </div>
     )
