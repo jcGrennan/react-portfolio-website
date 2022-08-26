d475a6304e242dd46eacf31d95dbe596c9e0fb6f
JC Grennan <jgrennan94@gmail.com>
2022-08-26 17:35:59 +0100

    rendered work her also

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
+import Work from "../components/Work"
 import Footer from "../components/Footer"
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
         <div>
             <Navbar />
             <Hero home={true}/>
+            <Work />
             <Footer />
         </div>
     )
